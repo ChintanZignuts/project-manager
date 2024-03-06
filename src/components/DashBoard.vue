5c9376c69327dbaa7967269126a1834b04664205
chintanrupareliya <ckrupareliya2002@gmail.com>
2024-03-07 00:36:13 +0530

    from home

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
-import router from '@/router'
-import { useUsers } from '@/stores/users'
-import { ref } from 'vue'
-const usersStore = useUsers()
-const rail = ref(true)
-const drawer = ref(true)
+import router from "@/router";
+import NavBar from "./NavBar.vue";
+import { useUsers } from "@/stores/users";
+import { ref } from "vue";
+const usersStore = useUsers();
+const rail = ref(true);
+const drawer = ref(true);
 const handleLogout = async () => {
-  await usersStore.logout()
-  localStorage.removeItem('token')
-  router.push('/')
-}
-const routeToDashBoard=()=>{
-  router.push('/dashboard')
-}
+  await usersStore.logout();
+  localStorage.removeItem("token");
+  router.push("/");
+};
+const routeToDashBoard = () => {
+  router.push("/dashboard");
+};
 </script>
 <template>
   <v-card>
@@ -22,14 +23,20 @@ const routeToDashBoard=()=>{
         :rail="rail"
         permanent
         @click="rail = false"
+        class="rounded-lg"
       >
         <v-list-item
+          class="text-h5"
           prepend-avatar="https://cdn.vuetifyjs.com/docs/images/brand-kit/v-logo.svg"
           title="Project Manger"
           nav
         >
           <template v-slot:append>
-            <v-btn icon="mdi-chevron-left" variant="text" @click.stop="rail = !rail"></v-btn>
+            <v-btn
+              icon="mdi-chevron-left"
+              variant="text"
+              @click.stop="rail = !rail"
+            ></v-btn>
           </template>
         </v-list-item>
 
@@ -44,22 +51,24 @@ const routeToDashBoard=()=>{
             active
           ></v-list-item>
         </v-list>
-        <VList class=".bottom">
+        <VList class="bottom">
           <v-list-item
             prepend-icon="mdi-logout"
-            title="LogOut"
+            title="Logout"
             value="logout"
             @click.stop="handleLogout()"
           ></v-list-item>
         </VList>
       </v-navigation-drawer>
-      <v-main style="height: 100vh"></v-main>
+      <v-main style="height: 100vh">
+        <NavBar />
+      </v-main>
     </v-layout>
   </v-card>
 </template>
 <style scoped>
-.bottom{
-    position:absolute;
-    bottom:15px;
+.bottom {
+  position: absolute;
+  bottom: 15px;
 }
 </style>
