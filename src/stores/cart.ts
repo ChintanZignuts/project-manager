67277cb40253d928412e4e7868c6c67c533f20f3
ChitanZignuts <chintanr@zignuts.com>
2024-03-14 19:36:59 +0530

    market place added

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,22 +1,25 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { computed, ref, watch } from "vue";
 import type { Items, CartItem } from "@/Productlist";
+import { toast, type ToastOptions } from "vue3-toastify";
+
 export const useCartStore = defineStore("cart", () => {
-  //states
+  // states
   const cartItems = ref<CartItem[]>([]);
-  const ItemsInCart = ref<number>(0);
   const date = new Date();
   const currentDate = ref<string>(date.toDateString());
+  const currentCartItem = ref<CartItem[]>([]);
+  const ItemsInCart = computed(() => currentCartItem.value.length);
+
   function addToCart(item: Items) {
     const existingItem = cartItems.value.find(
-      (cartItem) => cartItem.Item.id === item.id
+      (cartItem) =>
+        cartItem.Item.id === item.id && cartItem.orderDate === currentDate.value
     );
 
     if (existingItem) {
-      // If item already exists, update its quantity
       existingItem.quantity++;
     } else {
-      // If item does not exist, add it to the cart with quantity 1
       const newItem = {
         orderDate: currentDate.value,
         Item: item,
@@ -24,20 +27,89 @@ export const useCartStore = defineStore("cart", () => {
       };
       cartItems.value.push(newItem);
     }
+    toast.success(`${item.title} added to cart`, {
+      autoClose: 1500,
+      type: "success",
+      pauseOnFocusLoss: false,
+      hideProgressBar: true,
+      position: toast.POSITION.BOTTOM_RIGHT,
+    } as ToastOptions);
   }
 
-  function removeFromCart(id: number) {}
+  const getCurrentCartItem = computed(() => {
+    return cartItems.value.filter(
+      (item) => item.orderDate === currentDate.value
+    );
+  });
 
+  watch(getCurrentCartItem, (newValue) => {
+    currentCartItem.value = newValue;
+  });
+  function incrementQuantity(item: CartItem) {
+    const cartItem = cartItems.value.find((cartItem) => cartItem === item);
+    if (cartItem) {
+      cartItem.quantity++;
+    }
+  }
+
+  // Function to decrement the quantity of an item in the cart
+  function decrementQuantity(item: CartItem) {
+    const cartItem = cartItems.value.find((cartItem) => cartItem === item);
+    if (cartItem && cartItem.quantity > 1) {
+      cartItem.quantity--;
+    }
+  }
+  const subtotal = computed(() => {
+    let subtotal = 0;
+    currentCartItem.value.forEach((item) => {
+      subtotal += item.quantity * item.Item.price;
+    });
+    return subtotal;
+  });
+
+  // Function to remove item from cart
+
+  function removeFromCart(id: number) {
+    const index = cartItems.value.findIndex(
+      (item) => item.Item.id === id && item.orderDate === currentDate.value
+    );
+    if (index !== -1) {
+      cartItems.value.splice(index, 1);
+      toast.success(`Item removed from cart`, {
+        autoClose: 1500,
+        type: "success",
+        pauseOnFocusLoss: false,
+        hideProgressBar: true,
+        position: toast.POSITION.BOTTOM_RIGHT,
+      } as ToastOptions);
+    } else {
+      toast.error(`Item not found in cart`, {
+        autoClose: 1500,
+        type: "error",
+        pauseOnFocusLoss: false,
+        hideProgressBar: true,
+        position: toast.POSITION.BOTTOM_RIGHT,
+      } as ToastOptions);
+    }
+  }
+
+  // Function to clear the cart
   function clearCart() {
-    cartItems.value = [];
+    cartItems.value = cartItems.value.filter(
+      (item) => item.orderDate !== currentDate.value
+    );
   }
 
   return {
     cartItems,
     ItemsInCart,
     currentDate,
+    currentCartItem: getCurrentCartItem,
     addToCart,
     removeFromCart,
     clearCart,
+    incrementQuantity,
+    decrementQuantity,
+    subtotal,
   };
 });
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,7 +1,380 @@
 <script setup lang="ts">
 import { useCartStore } from "@/stores/cart";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { toast, type ToastOptions } from "vue3-toastify";
 const cartStore = useCartStore();
+const tab = ref<string | null>(null);
+const router = useRouter();
+const headers = ref([
+  { title: "Product", align: "start", key: "product", width: "30%" },
+  { title: "Price", align: "end", key: "Item.price" },
+  { title: "Quantity", align: "center", key: "quantity" },
+  { title: "Total", align: "end", key: "total" },
+  { title: "", align: "center", key: "delete" },
+]);
+const items = [
+  {
+    title: "Dashboard",
+    disabled: true,
+  },
+  {
+    title: "Cart",
+    disabled: true,
+  },
+];
+const AddressData = ref({
+  name: "",
+  buildingNo: "",
+  street: "",
+  city: "",
+  areaCode: "",
+  contact: "",
+});
+const validationRules = {
+  nameRules: [(v: string) => !!v || "Name is required"],
+  buildingNoRules: [(v: string) => !!v || "Building No is required"],
+  cityRules: [(v: string) => !!v || "City is required"],
+  areaCodeRules: [(v: number) => !!v || "Area code is required"],
+  contactRules: [
+    (v: string) =>
+      (v && v.length <= 10) || "Contact must be 10 characters or less",
+  ],
+};
+const handlePlaceOrder = () => {
+  cartStore.clearCart();
+  toast.success(`Order placed successfully`, {
+    autoClose: 1500,
+    type: "success",
+    pauseOnFocusLoss: false,
+    hideProgressBar: true,
+    position: toast.POSITION.BOTTOM_RIGHT,
+  } as ToastOptions);
+  AddressData.value = {
+    name: "",
+    buildingNo: "",
+    street: "",
+    city: "",
+    areaCode: "",
+    contact: "",
+  };
+};
+const routeToMarketplace = () => {
+  router.push("/marketplace");
+};
+const decrement = () => {};
 </script>
 <template>
-  <h1>hello{{ cartStore.cartItems }}</h1>
+  <vContainer class="pb-sm-15 pb-10 h-100 overflow-y-auto" fluid>
+    <div>
+      <div class="mt-3 mb-6">
+        <div class="d-flex justify-space-between">
+          <div class="d-flex py-0 align-center">
+            <div>
+              <h3 class="text-h5 mb-2">Checkout</h3>
+              <v-breadcrumbs :items="items" class="pa-0 ml-n1">
+                <template v-slot:divider>
+                  <v-icon icon="mdi-chevron-right"></v-icon>
+                </template>
+              </v-breadcrumbs>
+            </div>
+          </div>
+        </div>
+      </div>
+      <VCard variant="outlined" class="bg-surface border">
+        <VCardText>
+          <v-tabs
+            v-model="tab"
+            color="primary"
+            height="68px"
+            class="rounded-lg"
+          >
+            <v-tab value="one" height="70px" class="rounded-lg">
+              <VIcon>mdi-cart-variant</VIcon>
+              <div>
+                <div class="ma-0 pa-0"><Span>Item Cart</Span></div>
+                <span
+                  class="text-subtitle-2 text-lightText text-medium-emphasis font-weight-medium d-block ml-5"
+                  >Product Summary</span
+                >
+              </div></v-tab
+            >
+            <v-tab value="two" height="70px" class="rounded-lg"
+              ><VIcon>mdi-note-text-outline</VIcon>
+              <div>
+                <div class="ma-0 pa-0"><Span>Billing</Span></div>
+                <span
+                  class="text-subtitle-2 text-lightText text-medium-emphasis font-weight-medium d-block ml-5"
+                >
+                  Billing Information
+                </span>
+              </div></v-tab
+            >
+          </v-tabs>
+
+          <v-card-text>
+            <v-window v-model="tab">
+              <v-window-item value="one">
+                <div>
+                  <h3 class="my-4">
+                    Cart Item of {{ cartStore.currentDate }}({{
+                      cartStore.ItemsInCart
+                    }})
+                  </h3>
+                  <VDivider />
+                  <v-data-table-virtual
+                    :headers="headers"
+                    :items="cartStore.currentCartItem"
+                    item-key="id"
+                    class="text-subtitle-1 font-weight-bold"
+                  >
+                    <template v-slot:item.product="{ item }">
+                      <div class="d-flex align-center">
+                        <v-avatar size="70" class="ma-5 rounded-lg">
+                          <v-img
+                            :src="item.Item.images[0]"
+                            alt="Product Image"
+                            cover
+                          />
+                        </v-avatar>
+                        <p>{{ item.Item.title }}</p>
+                      </div>
+                    </template>
+                    <template v-slot:item.Item.price="{ item }">
+                      ${{ item.Item.price }}
+                    </template>
+                    <template v-slot:item.quantity="{ item }">
+                      <v-btn-toggle width="100px">
+                        <v-btn
+                          @click="cartStore.decrementQuantity(item)"
+                          class="border"
+                        >
+                          <VIcon>mdi-minus</VIcon>
+                        </v-btn>
+                        <v-btn class="border">{{ item.quantity }}</v-btn>
+                        <!-- Display current value -->
+                        <v-btn
+                          @click="cartStore.incrementQuantity(item)"
+                          class="border"
+                        >
+                          <!-- Plus Button -->
+                          <VIcon>mdi-plus</VIcon>
+                        </v-btn>
+                      </v-btn-toggle>
+                    </template>
+                    <template v-slot:item.total="{ item }">
+                      ${{ item.quantity * item.Item.price }}
+                    </template>
+                    <template v-slot:item.delete="{ item }">
+                      <v-btn
+                        icon="mdi-trash-can-outline"
+                        class="elevation-0 text-orange-darken-1"
+                        @click="cartStore.removeFromCart(item.Item.id)"
+                      ></v-btn>
+                    </template>
+                  </v-data-table-virtual>
+                </div>
+                <VCard class="elevation-0 border rounded-lg">
+                  <VCardTitle>
+                    <h5 class="text-h5 mb-6">Order Summery</h5>
+                  </VCardTitle>
+                  <VCardText>
+                    <div class="d-flex align-center justify-space-between my-5">
+                      <h6 class="text-subtitle-1 font-weight-regular">
+                        Sub Total
+                      </h6>
+                      <h6 class="font-weight-semibold text-subtitle-1">
+                        $ {{ cartStore.subtotal }}
+                      </h6>
+                    </div>
+                    <div class="d-flex align-center justify-space-between my-5">
+                      <h6 class="text-subtitle-1 font-weight-regular">
+                        Discount 5%
+                      </h6>
+                      <h6
+                        class="font-weight-semibold text-subtitle-1 text-error"
+                      >
+                        - {{ (cartStore.subtotal / 100) * 5 }}
+                      </h6>
+                    </div>
+                    <div class="d-flex align-center justify-space-between my-5">
+                      <h6 class="text-subtitle-1 font-weight-regular">
+                        Shipping Charges
+                      </h6>
+                      <h6 class="font-weight-semibold text-subtitle-1">-</h6>
+                    </div>
+                    <div class="d-flex align-center justify-space-between my-5">
+                      <h6 class="text-subtitle-1">Total</h6>
+                      <h6 class="font-weight-semibold text-subtitle-1">
+                        $
+                        {{
+                          cartStore.subtotal - (cartStore.subtotal / 100) * 5
+                        }}
+                      </h6>
+                    </div>
+                  </VCardText>
+                </VCard>
+                <VRow class="mt-3">
+                  <VCol cols="12" sm="6">
+                    <VBtn variant="tonal" @click.stop="routeToMarketplace">
+                      Continue Shopping
+                    </VBtn>
+                  </VCol>
+                  <VCol class="text-sm-right" cols="12" sm="6">
+                    <VBtn
+                      class="bg-orange-accent-2 text-white"
+                      @click.stop="tab = 'two'"
+                    >
+                      CheckOut
+                    </VBtn>
+                  </VCol>
+                </VRow>
+              </v-window-item>
+
+              <v-window-item value="two">
+                <VRow>
+                  <VCol cols="12" lg="6" class="mt-4">
+                    <h4 class="text-h5 mb-5">Billing Address</h4>
+                    <form @submit.prevent="handlePlaceOrder">
+                      <VTextField
+                        v-model="AddressData.name"
+                        variant="outlined"
+                        label="Name"
+                        placeholder="First Name"
+                        :rules="validationRules.nameRules"
+                        required
+                      />
+                      <VTextField
+                        v-model="AddressData.buildingNo"
+                        variant="outlined"
+                        label="Building No"
+                        placeholder="Building No"
+                        :rules="validationRules.buildingNoRules"
+                        required
+                      />
+                      <VTextField
+                        v-model="AddressData.street"
+                        variant="outlined"
+                        label="Street"
+                        placeholder="Street"
+                      />
+
+                      <VTextField
+                        v-model="AddressData.city"
+                        variant="outlined"
+                        label="City"
+                        :rules="validationRules.cityRules"
+                        placeholder="City"
+                        required
+                      />
+                      <VRow>
+                        <VCol cols="12" md="6" class="ma-sm-0">
+                          <VTextField
+                            v-model="AddressData.areaCode"
+                            variant="outlined"
+                            label="Area code"
+                            placeholder="Area code"
+                            :rules="validationRules.areaCodeRules"
+                            type="number"
+                            required
+                          />
+                        </VCol>
+                        <VCol cols="12" md="6">
+                          <VTextField
+                            v-model="AddressData.contact"
+                            variant="outlined"
+                            label="Contact"
+                            :rules="validationRules.contactRules"
+                            placeholder="Contact"
+                            type="tel"
+                          />
+                        </VCol>
+                      </VRow>
+                      <VRow>
+                        <VCol cols="12" md="6">
+                          <VBtn
+                            class="w-100 text-none text-orange-accent-3 elevation-0"
+                            @click.stop="tab = 'one'"
+                          >
+                            Back
+                          </VBtn>
+                        </VCol>
+                        <VCol cols="12" md="6">
+                          <VBtn
+                            type="submit"
+                            class="w-100 text-none text-white bg-orange-accent-2 elevation-0"
+                          >
+                            Place Order
+                          </VBtn>
+                        </VCol>
+                      </VRow>
+                    </form>
+                  </VCol>
+                  <VCol cols="12" lg="6">
+                    <VCard class="elevation-0 border rounded-lg">
+                      <VCardTitle>
+                        <h5 class="text-h5 mb-6">Order Summery</h5>
+                      </VCardTitle>
+                      <VCardText>
+                        <div
+                          class="d-flex align-center justify-space-between my-5"
+                        >
+                          <h6 class="text-subtitle-1 font-weight-regular">
+                            Sub Total
+                          </h6>
+                          <h6 class="font-weight-bold text-subtitle-1">
+                            $ {{ cartStore.subtotal }}
+                          </h6>
+                        </div>
+                        <div
+                          class="d-flex align-center justify-space-between my-5"
+                        >
+                          <h6 class="text-subtitle-1 font-weight-regular">
+                            Discount 5%
+                          </h6>
+                          <h6
+                            class="font-weight-bold text-subtitle-1 text-error"
+                          >
+                            - {{ (cartStore.subtotal / 100) * 5 }}
+                          </h6>
+                        </div>
+                        <div
+                          class="d-flex align-center justify-space-between my-5"
+                        >
+                          <h6 class="text-subtitle-1 font-weight-regular">
+                            Shipping Charges
+                          </h6>
+                          <h6 class="font-weight-bold text-subtitle-1">-</h6>
+                        </div>
+                        <div
+                          class="d-flex align-center justify-space-between my-5"
+                        >
+                          <h6 class="text-subtitle-1">Total</h6>
+                          <h6 class="font-weight-bold text-subtitle-1">
+                            $
+                            {{
+                              cartStore.subtotal -
+                              (cartStore.subtotal / 100) * 5
+                            }}
+                          </h6>
+                        </div>
+                      </VCardText>
+                    </VCard></VCol
+                  >
+                  <VCol></VCol>
+                </VRow>
+              </v-window-item>
+            </v-window>
+          </v-card-text>
+        </VCardText>
+      </VCard>
+    </div>
+  </vContainer>
 </template>
+<style scoped>
+.tabs {
+  color: orangered; /* Change this to your desired text color */
+  text-transform: none;
+  text-transform: unset !important;
+}
+</style>
